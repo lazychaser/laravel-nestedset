4ec67ccab9b05f3a1ebaef017dec169ded6a2743
Aleksander Kalnoy <lazychaser@gmail.com>
2014-01-10 00:04:21 +0400

    Remove Collection::toDictionary

---
+++ CHANGELOG.markdown
@@ -0,0 +1,2 @@
+### 1.0.1
+*   Removed `Collection::toDictionary`. Use `Collection::groupBy`.
--- src/Kalnoy/Nestedset/Collection.php
+++ src/Kalnoy/Nestedset/Collection.php
@@ -5,57 +5,62 @@ use \Illuminate\Database\Eloquent\Collection as BaseCollection;
 class Collection extends BaseCollection {
 
     /**
-     * Convert list of nodes to dictionary with specified key.
-     *
-     * If no key is specified then "parent_id" is used.
-     *
-     * @param string $key 
-     *
-     * @return  array
-     */
-    public function toDictionary($key = null)
-    {
-        if (empty($this->items)) {
-            return array();
-        }
-
-        if ($key === null) {
-            $key = $this->first()->getParentIdName();
-        }
-
-        $result = array();
-
-        foreach ($this->items as $item) {
-            $result[$item->$key][] = $item;
-        }
-
-        return $result;
-    }
-
-    /**
-     * Build tree from node list.
+     * Build tree from node list. Each item will have set children relation.
      *
      * To succesfully build tree "id", "_lft" and "parent_id" keys must present.
-     * 
+     *
      * If {@link rootNodeId} is provided, the tree will contain only descendants
      * of the node with such primary key value.
-     * 
+     *
      * @param integer $rootNodeId
      *
      * @return  Collection
      */
     public function toTree($rootNodeId = null)
     {
-        $dictionary = $this->toDictionary();
         $result = new static();
 
+        if (empty($this->items)) return $result;
+
+        $key = $this->first()->getParentIdName();
+        $dictionary = $this->groupBy($key);
+
+        $rootNodeId = $this->getRootNodeId($rootNodeId);
+
+        if (!isset($dictionary[$rootNodeId]))
+        {
+            return $result;
+        }
+
+        $result->items = $dictionary[$rootNodeId];
+
+        foreach ($this->items as $item)
+        {
+            $key = $item->getKey();
+
+            $children = new BaseCollection(isset($dictionary[$key]) ? $dictionary[$key] : array());
+
+            $item->setRelation('children', $children);
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param null|int $rootNodeId
+     *
+     * @return int
+     */
+    public function getRootNodeId($rootNodeId = null)
+    {
         // If root node is not specified we take parent id of node with
         // least lft value as root node id.
-        if ($rootNodeId === null) 
+        if ($rootNodeId === null)
         {
             $leastValue = null;
 
-            foreach ($this->items as $item) {
+            foreach ($this->items as $item)
+            {
                 if ($leastValue === null || $item->getLft() < $leastValue)
                 {
                     $leastValue = $item->getLft();
@@ -64,21 +69,6 @@ class Collection extends BaseCollection {
             }
         }
 
-        $result->items = isset($dictionary[$rootNodeId]) ? $dictionary[$rootNodeId] : array();
-
-        if (empty($result->items)) 
-        {
-            return $result;
-        }
-
-        foreach ($this->items as $item) 
-        {
-            $key = $item->getKey();
-
-            $children = new BaseCollection(isset($dictionary[$key]) ? $dictionary[$key] : array());
-            $item->setRelation('children', $children);
-        }
-
-        return $result;
+        return $rootNodeId;
     }
 }
