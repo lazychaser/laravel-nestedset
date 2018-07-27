e7033a76481baa342dd3b130827746430dc46efc
Anton <sozidatel79@users.noreply.github.com>
2018-07-28 02:31:49 +0300

    Update QueryBuilder.php

--- src/QueryBuilder.php
+++ src/QueryBuilder.php
@@ -1109,24 +1109,4 @@ class QueryBuilder extends Builder
             'pageName' => $pageName,
         ]);
     }
-    
-    /**
-     * @param null $perPage
-     * @param array $columns
-     * @param string $pageName
-     * @param null $page
-     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator|\Illuminate\Pagination\LengthAwarePaginator
-     */
-    public function paginate($perPage = null, $columns = ['*'], $pageName = 'page', $page = null)
-    {
-        $page = $page ?: Paginator::resolveCurrentPage($pageName);
-         $perPage = $perPage ?: $this->model->getPerPage();
-         $results = ($total = $this->toBase()->getCountForPagination())
-            ? $this->forPage($page, $perPage)->get($columns)
-            : $this->model->newCollection();
-         return $this->paginator($results, $total, $perPage, $page, [
-            'path' => Paginator::resolveCurrentPath(),
-            'pageName' => $pageName,
-        ]);
-    }
 }
